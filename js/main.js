445a9ab4a25453d67fdef679250623da285ed444
Adam Leung <centribo@hotmail.com>
2019-02-13 19:52:05 -0500

    Completed main slides

--- js/main.js
+++ js/main.js
@@ -46,11 +46,46 @@ function previewInterval(a, b){
 	}
 }
 
-function playChord(quality, inversion = 0){
-	var notes = MusicTheory.getChord(48, quality, inversion);
+function previewChord(root, quality, inversion = 0){
+	var notes = MusicTheory.getChord(root, quality, inversion);
+	// Play individually
+	for (var i = 0; i < notes.length; i++){
+		MIDI.noteOn(0, notes[i], 127, i);
+		MIDI.noteOff(0, notes[i], 3);
+	}
+
+	// Play all at once:
+	for(var i = 0; i < notes.length; i++){
+		MIDI.noteOn(0, notes[i], 127, notes.length);
+		MIDI.noteOff(0, notes[i], notes.length+2);
+	}
+}
+
+function previewScale(root, quality){
+	var notes = [];
+
+	switch(quality){
+		case "Major":
+			notes = [root, root + 2, root + 4, root + 5, root + 7, root + 9, root + 11, root + 12,
+					root + 12, root + 11, root + 9, root + 7, root + 5, root + 4, root + 2, root];
+		break;
+		case "Natural Minor":
+			notes = [root, root + 2, root + 3, root + 5, root + 7, root + 8, root + 10, root + 12,
+					root + 12, root + 10, root + 8, root + 7, root + 5, root + 3, root + 2, root];
+		break;
+	}
+
+	for(var i = 0; i < notes.length; i++){
+		MIDI.noteOn(0, notes[i], 127, i);
+		MIDI.noteOff(0, notes[i], 127, i+1.5);
+	}
+}
+
+function playChord(root, quality, inversion = 0){
+	var notes = MusicTheory.getChord(root, quality, inversion);
 	console.log(notes);
 	for(var i = 0; i < notes.length; i++){
 		MIDI.noteOn(0, notes[i], 127, 0);
-		MIDI.noteOff(0, notes[i], 3);
+		MIDI.noteOff(0, notes[i], 1);
 	}
 }
